4fad1871a674fcad9ec530e1906c61511bfc53e5
Piyas Saha <piyassaha0@gmail.com>
2026-05-25 23:44:43 +0600

    Initial: Creating Page with animal name

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from fpdf import FPDF
+from glob import glob
+from pathlib import Path
+
+filepaths = glob("Texts/*.txt")
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for filepath in filepaths:
+    df = pd.read_csv(filepath)
+    filename = Path(filepath).stem
+    pdf.add_page()
+    pdf.set_font(family= "Times",style= "B",size= 16)
+    pdf.cell(w=50,h=8,txt=f"{filename}")
+
+pdf.output("PDFs/Animal.pdf")
