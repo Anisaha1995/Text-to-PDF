470aec0bd8447d812f127fb2ed2de2aa923c7e06
Piyas Saha <piyassaha0@gmail.com>
2026-05-26 13:04:46 +0600

    Content add

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     pdf.add_page()
     pdf.set_font(family= "Times",style= "B",size= 16)
-    pdf.cell(w=50,h=8,txt=f"{filename.title()}")
+    pdf.cell(w=50,h=8,txt=f"{filename.title()}", ln=1)
+    with open(filepath,'r') as file:
+        content = file.read()
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("PDFs/Animal.pdf")
